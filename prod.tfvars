8d96f4a5661c4ea3daca89d2a1d51fb853de2249
akash <akash@gmail.com>
2023-12-23 23:50:31 +0000

    Infra version 1

---
+++ prod.tfvars
@@ -0,0 +1,6 @@
+instance_type = "t2.micro"
+project_name = "shopping-app"
+project_env = "prod"
+hostname_domain_name = "sproadways.online"
+hosted_zone_id = "Z0132167LLIYWXTY364M"
+hostname = "shopping-app"
---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "instance_type" {
+type = string
+description = "Type of instance"
+}
+variable "project_name" {
+type = string
+description = "Project name"
+}
+variable "project_env" {
+type = string
+description = "Environment of the project"
+}
+variable "hosted_zone_id" {
+type = string
+}
+variable "hostname" {
+type = string
+description = "Hostname"
+}
